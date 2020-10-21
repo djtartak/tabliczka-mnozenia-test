776c0fb984d2674393c071f1d48bf73b85ab0ae1
djtartak <jaroslaw.dzedzej@gmail.com>
2020-10-21 22:14:58 +0200

    dodana aktualizacja magazynu

--- rodzaje_importu/sklep/tworzenie_zamowienia.py
+++ rodzaje_importu/sklep/tworzenie_zamowienia.py
@@ -10,6 +10,10 @@ def stwoz_zamowienie(produkt, ilosc):
     cena = produkty[produkt]["price"]
     cena_razem = cena * ilosc
 
+    if ilosc > produkty[produkt]["quantity"]:
+        print('nie mamy aż tyle produktu',produkt)
+
+
     nowe_zamowienie = {
         "produkt": produkt,
         "cena_razem": cena_razem,
@@ -17,6 +21,10 @@ def stwoz_zamowienie(produkt, ilosc):
                    }
     zamowienia.append(nowe_zamowienie)
     return nowe_zamowienie
+
+def zaktualizuj_magazyn(produkt,ilosc):
+    if ilosc < produkty[produkt]["quantity"]:
+        produkty[produkt]["quantity"] -= ilosc
 
 
 #stwoz_zamowienie(produkt, ilosc)
--- rodzaje_importu/sklep_main.py
+++ rodzaje_importu/sklep_main.py
@@ -1,9 +1,13 @@
+from sklep.magazyn import produkty
 from sklep.tworzenie_zamowienia import stwoz_zamowienie
 from sklep.tworzenie_zamowienia import zamowienia
+from sklep.tworzenie_zamowienia import zaktualizuj_magazyn
 
 produkt = input("podaj produkt (chleb lub masło)")
 ilosc = int(input("podaj ilość do zamówienia"))
 
 
 print(stwoz_zamowienie(produkt, ilosc))
+zaktualizuj_magazyn(produkt, ilosc)
 print(zamowienia)
+print("aktualnie w magazynie jest:",produkty)
