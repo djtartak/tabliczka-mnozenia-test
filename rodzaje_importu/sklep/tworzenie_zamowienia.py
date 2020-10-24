30bf1d0ffed0fa43a1b4692f73af70ff74ea6e20
djtartak <jaroslaw.dzedzej@gmail.com>
2020-10-24 21:32:00 +0200

    zaawansowanie komunikatów oraz główna pętla sklepu

--- rodzaje_importu/sklep/tworzenie_zamowienia.py
+++ rodzaje_importu/sklep/tworzenie_zamowienia.py
@@ -6,25 +6,28 @@ produkt = "chleb"
 ilosc = 10
 
 
+
 def stwoz_zamowienie(produkt, ilosc):
     cena = produkty[produkt]["price"]
     cena_razem = cena * ilosc
-
-    if ilosc > produkty[produkt]["quantity"]:
-        print('nie mamy aż tyle produktu',produkt)
-
 
     nowe_zamowienie = {
         "produkt": produkt,
         "cena_razem": cena_razem,
         "ilosc": ilosc,
-                   }
+    }
+
+    #if ilosc > produkty[produkt]["quantity"]:
+     #   print('nie mamy aż tyle produktu',produkt)
+
     zamowienia.append(nowe_zamowienie)
     return nowe_zamowienie
 
 def zaktualizuj_magazyn(produkt,ilosc):
-    if ilosc < produkty[produkt]["quantity"]:
+    if ilosc <= produkty[produkt]["quantity"]:
         produkty[produkt]["quantity"] -= ilosc
+    else:
+        print('nie mamy aż tyle produktu', produkt)
 
 
 #stwoz_zamowienie(produkt, ilosc)
