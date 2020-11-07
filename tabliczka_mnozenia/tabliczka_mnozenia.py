7c67e5332483ed08a257140a60f0afef51d8e07a
djtartak <jaroslaw.dzedzej@gmail.com>
2020-11-07 17:41:43 +0100

    tabliczka mnozenia dla Tytusa poprawiona interaktywność w pętli

--- tabliczka_mnozenia/tabliczka_mnozenia.py
+++ tabliczka_mnozenia/tabliczka_mnozenia.py
@@ -12,22 +12,44 @@ def sprawdzenie_wyniku(wynik):
 
 
 def wylosuj_mnoznik():
-    a = random.randint(1,10)
+    a = random.randint(2,9)
     return a
 
 
+
 kontynuuj="t"
+
+# główna pęta programu
 while kontynuuj == "t":
+    punkty = 0
+    while punkty<10:
 
+        x = wylosuj_mnoznik()
+        y = wylosuj_mnoznik()
 
-    x = wylosuj_mnoznik()
-    y = wylosuj_mnoznik()
+        print('podaj wynik mnozenia:',x,'razy',y)
 
-    print('podaj wynik mnozenia:',x,'razy',y)
+        wynik = int(input())
+        if sprawdzenie_wyniku(wynik)==True:
+            print ('gratulacje, poprawna odpowiedź, zdobywasz punkt')
+            punkty +=1
 
-    wynik = int(input())
+        else:
+            while sprawdzenie_wyniku(wynik)==False:
+                print('niestety to zła odpowiedż spróbuj jeszcze raz')
+                wynik = int(input())
+            else:
+                print ('gratulacje, poprawna odpowiedź, zdobywasz punkt')
+                punkty += 1
+        print('twoj aktualny wynik = ', punkty, 'jeszcze', 10 - punkty, 'poprawnych odpowiedzi i wygrasz')
+       # kontynuuj = input('chcesz kontynuować t/n?')
 
-    print (sprawdzenie_wyniku(wynik))
+    print ('brawo  ! zdobyłeś 10 punktów !')
+    print ('jestes MISTRZEM TABLCZKI MNOZENIA do 100 !')
+    print(' + + + +  ')
+
+    print('|\/\/\/\|')
+    print('|       |')
+    print('---------')
 
     kontynuuj = input('chcesz kontynuować t/n?')
-
